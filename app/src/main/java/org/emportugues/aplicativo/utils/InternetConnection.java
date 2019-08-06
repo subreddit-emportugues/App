bf5874862a05a7666bfb074e1ed290dfbf22100b
JorgeAmVF <javf.pub@icloud.com>
2019-08-06 10:14:36 -0300

    Update SDK

--- app/src/main/java/org/emportugues/aplicativo/utils/InternetConnection.java
+++ app/src/main/java/org/emportugues/aplicativo/utils/InternetConnection.java
@@ -2,7 +2,8 @@ package org.emportugues.aplicativo.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
 
 import java.util.Objects;
 
